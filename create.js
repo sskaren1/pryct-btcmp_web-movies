9301159920e9c8a728c68e066d9c4413b373a56d
sskaren1 <karen.93st@gmail.com>
2022-03-19 01:13:19 -0500

    first commit

---
+++ create.js
@@ -0,0 +1,72 @@
+// paso 1: Obtener el valor de inputs
+const btnCrear = document.querySelector("#btn-crear");
+const inputs = document.querySelectorAll("input");
+const selectGender = document.querySelector("#select-gender");
+// vamos a crear una variable la cual se encargue de almacenar el valor del onchange
+// del select
+let valueGender = "";
+
+selectGender.onchange = function (event) {
+  valueGender = event.target.value;
+};
+
+btnCrear.onclick = async function (event) {
+  // como evitamos que la pagina se recargue
+  event.preventDefault();
+  selectGender.style.border = "1px solid #ccc";
+
+  // al momento de dar click debo obtener el valor de los inputs
+  const values = {};
+  // Esto sirve para poder agregar un elemento a un objeto
+  // values["key"] = "value"
+  // values = {
+  //  "key": "value"
+  // }
+
+  // ahora debemos validar que los inputs y el select esten llenos
+  // vamos a crear una varaible que sea true  y si en el forEach hay algun campo vacio esta pasa a ser false
+  let validacionInputs = true;
+  inputs.forEach((input) => {
+    input.style.border = "1px solid #ccc";
+
+    if (input.value === "") {
+      validacionInputs = false;
+      input.style.border = "1px solid #f01";
+    }
+
+    values[input.name] = input.value;
+  });
+
+  if (valueGender === "") {
+    selectGender.style.border = "1px solid #f01";
+    validacionInputs = false;
+  }
+
+  if (!validacionInputs) {
+    Swal.fire({
+      icon: "error",
+      title: "Error",
+      text: "Debe completar los campos",
+    });
+    return;
+  }
+
+  // para poder crear nuestro elemento en el API necesitamos armar un objeto
+  // este objeto sera que el que enviaremos al API para que pueda crear una nueva pelicula
+  const movie = {
+    name: values.movie_name,
+    director: values.movie_director,
+    wallpaper: values.movie_photo,
+    gender: valueGender,
+    video_link: values.movie_video,
+  };
+  await storeMovie(movie);
+  // Ahora vamos a ejecutar una peticion con el motodo POST porque vamos a crear algo
+  // Sabemos que por default el fetch es de metodo GET
+  // ahora para cambiarlos a un POST necesitamos pasarle un objeto indicando su METODO
+  // al decirle que method ahora es POST automaticamente podremos pasarle un body
+  // body: Es el objeto que vamos a enviar al servidor para que pueda ser creado
+  // body solo recibe strings
+
+  // Recuerden que es recomendable tener un try catch cuando hagamos una peticion
+};
---
+++ index.js
@@ -0,0 +1,58 @@
+// haremos una funcion que se encargue de agrupar las pelicualas
+const groupMovies = async () => {
+  // podemos crear 3 arreglos
+  // arreglo 1 para las peliculas de marvel
+  // arreglo 2 para las peliculas de DC
+  // arreglo 3 para las peliculas ramdon
+  const marvelMovies = [];
+  const dcMovies = [];
+  const ramdonMovies = [];
+
+  // vamos a obtener el arreglo de peliculas desde la funcion getMovies()
+  const movies = await getMovies();
+
+  movies.forEach((movie) => {
+    // compo podemos extraer el gender si usamos destructuracion?
+    const { gender } = movie;
+    if (gender === "Marvel") {
+      marvelMovies.push(movie);
+    } else if (gender === "DC") {
+      dcMovies.push(movie);
+    } else {
+      ramdonMovies.push(movie);
+    }
+  });
+
+  renderMovies(marvelMovies, "Marvel");
+  renderMovies(dcMovies, "DC");
+  renderMovies(ramdonMovies, "Ramdon");
+};
+
+// y otra funcion que se encargue de renderizar
+const containerMovies = document.querySelector("#container-videos");
+
+const renderMovies = (arregloPeliculas, titulo) => {
+  const html = `
+    <div class="col-md-12 mt-3">
+        <h4 class="gender-movie">
+        ${arregloPeliculas.length > 0 ? titulo : ""}
+        </h4>
+        <div class="row">
+        ${arregloPeliculas.map(
+          (pelicula) =>
+            `<div class="col-md-3">
+            <video onmouseover="this.play()" onmouseout="this.pause()" controls muted poster=${pelicula.wallpaper} class="video">
+              <source src=${pelicula.video_link} />
+            </video>
+            <h5 class="title">${pelicula.name}</h5>
+            <p class="subtitle">${pelicula.director}</p>
+          </div>`
+        )}
+        </div>
+    </div>
+      `;
+
+  containerMovies.innerHTML += html.replaceAll(",", "");
+};
+
+groupMovies();
